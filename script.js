afa4226b61678ee26736dc7bc657de7d87c441e0
RedonMehmeti <redonmehmeti2019@gmail.com>
2024-04-18 09:25:55 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+var x = 5;
+var y = 6;
+var z = x + y;
+
+document.getElementById("demo").innerHTML = "The value of z is:" + z;
+
+
+
+let age = 25;
+let price = 21.99;
+let gpa = 1.5;
+
+console.log(`Your are ${age} years old `);
+console.log(`The price is: ${price}`);
+console.log(`The gpa is: ${gpa}`);
+
+
+
+let online = false;
+let forsale = true;
+let isStudent = true;
+
+console.log(`bro is online ${online}`);
+console.log(`Is this car for sale ${forsale}`);
+console.log(`Redoni ${isStudent}`);
+
+
+
+let fullName = "Bro Code"
+let ages = 25;
+let isAstudent = true;
+
+document.getElementById("p1").innerHTML = fullName;
+document.getElementById("p2").innerHTML = ages;
+document.getElementById("p3").innerHTML = isAstudent;
+
+
+
+
+let names = "redon";
+let agess = 17;
+let email = "reda12@gmail.com";
+
+console.log(`Your name is: ${names}`)
+console.log(`Your ages is: ${agess}`)
+console.log(`Your email is: ${email}`)
+
+// Variablat janë kontejnerë për ruajtjen e informacionit. Krijimi i një variabli në JavaScript quhet "deklarimi" i një ndryshoreje: var carName; Pas deklarimit, ndryshorja është bosh (nuk ka vlerë).
